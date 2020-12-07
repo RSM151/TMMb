656902e5f4ac10ae45bf3888f040420bccaee7e1
Richard Musca <richardmuscafl1@gmail.com>
2020-12-06 23:15:28 -0500

    move api stuff to server

--- server/App.js
+++ server/App.js
@@ -17,11 +17,53 @@ app.post('/', (req, res) => {
     res.send(200);
 })
 
+async function getMovie(id, apiKey) {
+    let movie;
+    await fetch("https://api.themoviedb.org/3/movie/" + id + "?api_key=" + apiKey).then(response => response.json()).then(data => movie = data);
+    return movie;
+}
+
 app.post('/api', (req, res) => {
+
+    res.send(200);
     const query = req.body['query'];
     const type = req.body['type'];
     const filter = req.body['filter'];
     const dataStructure = req.body['dataStructure'];
+
+    const apiKey = "879743dbc28d0f7ee9a56559198a3c57";
+    let URL = "https://api.themoviedb.org/3/search/"
+    let movies;
+    // Handle movies
+    if (type === 'Movie') {
+        URL += "movie?api_key=" + apiKey + "&query=" + query + "&include_adult=true";
+        console.log(URL);
+        let movieIDs = [];
+        fetch(URL).then(response => response.json()).then(function (data) {
+
+            data['results'].forEach(movie => {
+                movieIDs.push(movie['id']);
+            });
+
+            movies = movieIDs.map((id) => getMovie(id, apiKey));
+
+            Promise.all(movies).then(function (values) {
+                let out = JSON.stringify(values);
+                fetch('http://localhost:8080/', {
+                    method: "POST",
+                    mode: 'cors',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(values)
+                }).then((response) => console.log(response.status));
+                //fetch('http://rmuscapi:8080').then((response) => console.log(response));
+            });
+
+        });
+    }
+
+
 });
 
 
